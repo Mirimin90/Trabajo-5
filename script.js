89b4e603a80ab9ddc0697ecf9de1b18419aacc5e
Miriam Pulgar <miriamconstanzape@gmail.com>
2023-03-29 22:32:20 -0300

    trabajo5

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+var peso
+var estatura
+var IMC
+
+peso = 58;
+estatura = 1.48;
+IMC = peso/(estatura*estatura);
+
+console.log(IMC);
+
+
+
+var producto
+var IVA
+var ValorIva
+
+producto = 15990;
+IVA = 0.19;
+ValorIva = producto*IVA;
+
+console.log(ValorIva);
+
+
+var cateto1
+var cateto2
+var hipotenusacuadrado
+var hipotenusa
+
+cateto1 = 4;
+cateto2 = 3;
+hipotenusacuadrado = (cateto1*cateto1) + (cateto2*cateto2)
+hipotenusa = Math.pow(hipotenusacuadrado, 0,5)
+
+alert(hipotenusa)
